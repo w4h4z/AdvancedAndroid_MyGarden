2e7d188c83901e0e0ae4493bec0286ff2f3bbd14
w4h4z <rizaldi.wahaz@gmail.com>
2017-11-08 14:03:58 +0700

    Add New Widget

---
+++ app/src/main/java/com/example/android/mygarden/provider/PlantWidgetProvider.java
@@ -0,0 +1,47 @@
+package com.example.android.mygarden.provider;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.example.android.mygarden.R;
+import com.example.android.mygarden.ui.MainActivity;
+
+/**
+ * Created by wahaz on 08/11/2017.
+ */
+public class PlantWidgetProvider extends AppWidgetProvider {
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appwidgetId) {
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.plant_widget);
+        views.setOnClickPendingIntent(R.id.widget_plant_image, pendingIntent);
+        appWidgetManager.updateAppWidget(appwidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+
+    }
+
+}
